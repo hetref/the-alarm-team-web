6e623bdfacc9e025f6471177099fbca36ee51c9a
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2025-12-03 20:22:30 +0530

    Given changes applied

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "Lynx Security" );
+define( "RECIPIENT_EMAIL", "support@lynxsecurity.net" );
 
 // Read the form values
 $success = false;
